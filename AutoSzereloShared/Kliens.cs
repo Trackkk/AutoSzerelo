0d8098c7241677a88cfc9db612c18f7a5315113d
Délczeg Sándor Balázs <track@mailbox.unideb.hu>
2024-05-25 15:14:46 +0200

    Implemented data to be handled in AutoSzereloShared

--- AutoSzereloShared/Kliens.cs
+++ AutoSzereloShared/Kliens.cs
@@ -1,7 +1,22 @@
-﻿namespace AutoSzereloShared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoSzereloShared
 {
     public class Kliens
     {
+        public Guid KliensId { get; set; }
 
+        [Required]
+        [RegularExpression(@"\S")]
+        public string KliensNev {get; set; }
+
+        [Required]
+        [RegularExpression(@"\S")]
+        public string Lakcim { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [RegularExpression(@"\S")]
+        public string Email { get; set; }
     }
 }
